3eff9b4c1af39fb164ed2d8784b2939aa6c51601
JACJava <juliejava@icloud.com>
2019-06-02 18:26:59 -0500

    Reading an HTTP Request

--- src/main/java/com/mantiso/SimpleServlet.java
+++ src/main/java/com/mantiso/SimpleServlet.java
@@ -14,7 +14,37 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.getWriter().write("Hello World Julie");
+        String name = req.getParameter("name");
 
+        //resp.getWriter().write("Hello World Julie");
+
+        if(name != null) {
+            resp.setContentType("text/xml");
+            resp.getWriter().printf("<name>Hello %s</name>", name);
+        } else {
+            resp.getWriter().write("Please enter a valid name");
+        }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+
+        //resp.getWriter().write("Hello World Julie");
+
+        if(name != null && name != "") {
+            resp.getWriter().printf("Hello %s", name);
+        } else {
+            //resp.getWriter().write("Please enter a valid name");
+            resp.sendRedirect("index.jsp");
+        }
+    }
+
+
+
+
+
+
+
 }
