3cbd0d8b66588030871d2c55987c2f5c408eb609
Julie Java <juliejava@icloud.com>
2019-06-03 18:13:22 -0500

    Finished Writing Servlets

--- src/main/java/com/mantiso/SimpleServlet.java
+++ src/main/java/com/mantiso/SimpleServlet.java
@@ -1,15 +1,25 @@
 package com.mantiso;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home")
+@WebServlet(urlPatterns = {"/home", "*.do"}, //note web.xml overrides this so *.do doesn't work
+        name="SimpleServlet",
+        initParams = {@WebInitParam(name = "ProductName", value="Welcome Application")})
 
 public class SimpleServlet extends HttpServlet {
+    String appName = "My Application";
+
+    @Override
+    public void init() throws ServletException {
+        //appName = getInitParameter("ProductName");
+        appName = getServletContext().getInitParameter("ProductName");
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -20,7 +30,13 @@ public class SimpleServlet extends HttpServlet {
 
         if(name != null) {
             resp.setContentType("text/xml");
-            resp.getWriter().printf("<name>Hello %s</name>", name);
+            //resp.getWriter().printf("<name>Hello there %s</name>", name);
+
+            resp.getWriter().printf("<application>" +
+                    "<name>Hello %s</name>" +
+                    "<product>%s</product>" +
+                    "</application>", name, appName);
+
         } else {
             resp.getWriter().write("Please enter a valid name");
         }
@@ -34,7 +50,7 @@ public class SimpleServlet extends HttpServlet {
         //resp.getWriter().write("Hello World Julie");
 
         if(name != null && name != "") {
-            resp.getWriter().printf("Hello %s", name);
+            resp.getWriter().printf("Hello there %s", name);
         } else {
             //resp.getWriter().write("Please enter a valid name");
             resp.sendRedirect("index.jsp");
